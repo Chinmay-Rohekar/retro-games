cd4a4a74ffe4b943a9d34cc2d49401fdc9d150b8
chinmay <chinmayrohekar123@gmail.com>
2026-01-05 11:08:48 +0530

    Tenth Commit

--- 04_memory/src/game_screen.py
+++ 04_memory/src/game_screen.py
@@ -3,19 +3,19 @@ import pygame as pg
 from button import Button
 from colors import colors, screen_dims
 
-class MenuScreen:
+class GameScreen:
     def __init__(self, in_screen):
         # Font
         self.screen = in_screen
         self.font = pg.font.SysFont(None, 60)
-        self.button1 = Button("Start Game", (170, 60), (screen_dims['width']/2, 200), colors['GREEN'],
+        self.button1 = Button("Pause Game", (170, 60), (screen_dims['width']/2, 200), colors['GREEN'],
                               colors['GRAY'], self.screen, 40)
         self.button2 = Button("Quit Game", (170, 60), (screen_dims['width']/2, 300), colors['GREEN'],
                               colors['GRAY'], self.screen, 40)
 
     def draw_screen(self):
         self.screen.fill(colors['RED'])
-        text_start = self.font.render('MENU', True, colors['BLUE'])
+        text_start = self.font.render('GAME', True, colors['BLUE'])
         rect_start = text_start.get_rect()
         rect_start.center = screen_dims['width']/2, 50
         self.screen.blit(text_start, rect_start)
--- 04_memory/src/main.py
+++ 04_memory/src/main.py
@@ -1,6 +1,8 @@
 # Import the necessary libraries
 import pygame as pg
 from menu_screen import MenuScreen
+from game_screen import GameScreen
+from pause_screen import PauseScreen
 from colors import colors, screen_dims
 
 
@@ -11,13 +13,14 @@ screen = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption("Memory Tiles")
 # Timer Variables
 clock = pg.time.Clock()
-
+# Game Variables
+game_on = False
 player_score = 0
 
 # Screens
 menuScreen = MenuScreen(screen)
-# gameScreen = GameScreen(screen)
-# pauseScreen = PauseScreen(screen)
+gameScreen = GameScreen(screen)
+pauseScreen = PauseScreen(screen)
 # scoreScreen = ScoreScreen(screen)
 screens = {'menu': menuScreen}
 # screens = {'menu': menuScreen, 'game': gameScreen, 'pause': pauseScreen, 'score': scoreScreen}
@@ -30,6 +33,20 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # Handle Button Clicks for Various Screens
+            mouse_x, mouse_y = pg.mouse.get_pos()
+            if current_screen == menuScreen:
+                if menuScreen.button1.rect_button.collidepoint(mouse_x, mouse_y):
+                    current_screen = screens['game']     # Start Game
+                elif menuScreen.button2.rect_button.collidepoint(mouse_x, mouse_y):
+                    running = False     # Quit Game
+            elif current_screen == gameScreen:
+                if gameScreen.button1.rect_button.collidepoint(mouse_x, mouse_y):
+                    current_screen = screens['pause']     # Pause Game
+                elif gameScreen.button2.rect_button.collidepoint(mouse_x, mouse_y):
+                    running = False     # Quit Game
 
     screen.fill(colors['WHITE'])
     current_screen.draw_screen()
