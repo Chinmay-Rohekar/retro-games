8763764b04a7b33e562dea9f7641ac91a43d6ac2
chinmay <chinmayrohekar123@gmail.com>
2026-01-09 18:47:22 +0530

    12th Commit

--- 05_battle_tank/src/game_screen.py
+++ 05_battle_tank/src/game_screen.py
@@ -7,13 +7,16 @@ class GameScreen:
         self.screen = in_screen
         self.font = pg.font.SysFont(None, 30)
         self.image = pg.transform.scale(pg.image.load("..//resources//game_screen_image.png"),
-                                        screen_dims)
+                                        screen_dims).convert_alpha()
+        self.image.set_alpha(200)
         self.score_text = self.font.render("SCORE: 0", True, colors['WARM_GOLD'])
         self.score_rect = self.score_text.get_rect(center=(screen_dims[0] // 2, 25))
         self.home_image = pg.transform.scale(pg.image.load("..//resources//home_icon.png"),
                                              (40, 40))
         self.pause_image = pg.transform.scale(pg.image.load("..//resources//pause_icon.png"),
                                              (40, 40))
+        self.tank_image = pg.transform.scale(pg.image.load("..//resources//tank_red.png"),
+                                             (75, 120))
 
     def draw_screen(self):
         self.screen.fill(colors['WHITE'])
@@ -26,6 +29,7 @@ class GameScreen:
         pg.draw.rect(self.screen, colors['BLACK'], rect=(250, 5, 40, 40), border_radius=5)
         self.screen.blit(self.home_image, (250, 5))
         self.screen.blit(self.pause_image, (10, 5))
+        self.screen.blit(self.tank_image, (150, 250))
         self.screen.blit(self.score_text, self.score_rect)
 
     def start_music(self):
